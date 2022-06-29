b1f023b1db7106c77633c496053caffce97563d6
Tres-Ales <55411526+Tres-Ales@users.noreply.github.com>
2022-06-29 11:02:12 +0300

    Delete Database1 directory

--- Database1/Database1/dbo/Tables/Product1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Product1] (
-    [productid]   INT        NULL,
-    [productName] NCHAR (20) NULL,
-    [Cost]        INT        NULL
-);
-
--- Database1/Database1/dbo/Tables/User1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[User1] (
-    [userid]   INT        NULL,
-    [username] NCHAR (20) NULL,
-    [password] NCHAR (30) NULL
-);
-
